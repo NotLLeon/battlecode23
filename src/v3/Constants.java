4fc216f6bfe40bfc50eada7f177130bbd8f6258f
Daniel Li <daniel.lisq12@gmail.com>
2023-01-14 20:19:47 -0500

    v3 init commit

---
+++ src/v3/Constants.java
@@ -0,0 +1,15 @@
+package v2;
+
+public class Constants {
+    // weights for explore
+    static int TOTAL_WEIGHT_DIRECTIONS = 13;
+    static int HIGH_WEIGHT_DIRECTION = 6;
+    static int MID_WEIGHT_DIRECTION = 2;
+
+    // stop sampling direction
+    static int MAX_DIRECTION_SEARCH_ATTEMPTS = 32;
+
+    // indices for comms
+    static int IDX_NUM_HQS = 63;
+    static int NUM_WELLS_STORED;
+}
---
+++ src/v3/Headquarters.java
@@ -0,0 +1,56 @@
+package v2;
+
+import battlecode.common.*;
+
+import java.util.Random;
+
+public class Headquarters extends Robot {
+
+    static final Random rng = new Random(6147);
+
+    static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+
+    static int index = 0;
+
+    static void runHeadquarters(RobotController rc, int turnCount) throws GameActionException {
+        // Pick a direction to build in.
+        Direction dir = directions[rng.nextInt(directions.length)];
+        MapLocation curLoc = rc.getLocation();
+        MapLocation newLoc = curLoc.add(dir);
+//        if (rc.canBuildAnchor(Anchor.STANDARD)) {
+//            // If we can build an anchor do it!
+//            rc.buildAnchor(Anchor.STANDARD);
+//            rc.setIndicatorString("Building anchor! " + rc.getAnchor());
+//        }
+
+        if(turnCount == 1) {
+           Comms.writeHQ(rc, rc.getLocation());
+        }
+
+        int raw = rc.readSharedArray(index)-1;
+        rc.setIndicatorString("Index: " + index + " Location: (" + (raw%rc.getMapWidth()) + "," + (raw/rc.getMapWidth()));
+
+        if (rng.nextBoolean()) {
+            // Let's try to build a carrier.
+           // rc.setIndicatorString("Trying to build a carrier");
+            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
+                rc.buildRobot(RobotType.CARRIER, newLoc);
+            }
+        } else {
+            // Let's try to build a launcher.
+            rc.setIndicatorString("Trying to build a launcher");
+            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
+                rc.buildRobot(RobotType.LAUNCHER, newLoc);
+            }
+        }
+    }
+}
