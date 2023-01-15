ab2eacb7426481fc0fbb783656b402a3b71ed72d
Felix <felix.yang0917@gmail.com>
2023-01-15 15:34:17 -0500

    copy v3Capped and added amplifiers

---
+++ src/v3_1/Random.java
@@ -0,0 +1,50 @@
+package v3_1;
+
+import battlecode.common.Direction;
+
+public class Random {
+    
+    static java.util.Random rng = new java.util.Random(6147);
+
+    static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+
+    static int getDirectionOrderNum(Direction dir) {
+        for (int i = 0; i < 8; ++i) {
+            if (directions[i] == dir) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    static Direction nextDir() {
+        return directions[rng.nextInt(directions.length)];
+    }
+
+    static Direction nextDirWeighted(int weights[], int totalWeight) {
+        int idx = 0;
+        for (int i = nextInt(totalWeight); idx < directions.length - 1; ++idx) {
+            i -= weights[idx];
+            if (i <= 0) break;
+        }
+        return directions[idx];
+    }
+
+    static int nextInt(int bound) {
+        return rng.nextInt(bound);
+    }
+
+    static boolean nextBoolean() {
+         return rng.nextBoolean();
+    }
+
+}
