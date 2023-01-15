629345d2c722f17dedf6b1dc2b5eb0f4d6265387
Leon Liu <leonyi.liu@gmail.com>
2023-01-14 20:53:06 -0500

    Cap total number of units

--- src/v3/Headquarters.java
+++ src/v3/Headquarters.java
@@ -2,28 +2,12 @@ package v3;
 
 import battlecode.common.*;
 
-import java.util.Random;
-
 public class Headquarters extends Robot {
-
-    static final Random rng = new Random(6147);
-
-    static final Direction[] directions = {
-            Direction.NORTH,
-            Direction.NORTHEAST,
-            Direction.EAST,
-            Direction.SOUTHEAST,
-            Direction.SOUTH,
-            Direction.SOUTHWEST,
-            Direction.WEST,
-            Direction.NORTHWEST,
-    };
-
     static int index = 0;
 
     static void runHeadquarters(RobotController rc, int turnCount) throws GameActionException {
         // Pick a direction to build in.
-        Direction dir = directions[rng.nextInt(directions.length)];
+        Direction dir = Random.nextDir();
         MapLocation curLoc = rc.getLocation();
         MapLocation newLoc = curLoc.add(dir);
 //        if (rc.canBuildAnchor(Anchor.STANDARD)) {
@@ -39,17 +23,19 @@ public class Headquarters extends Robot {
         int raw = rc.readSharedArray(index)-1;
         rc.setIndicatorString("Index: " + index + " Location: (" + (raw%rc.getMapWidth()) + "," + (raw/rc.getMapWidth()));
 
-        if (rng.nextBoolean()) {
-            // Let's try to build a carrier.
-           // rc.setIndicatorString("Trying to build a carrier");
-            if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
-                rc.buildRobot(RobotType.CARRIER, newLoc);
-            }
-        } else {
-            // Let's try to build a launcher.
-            rc.setIndicatorString("Trying to build a launcher");
-            if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
-                rc.buildRobot(RobotType.LAUNCHER, newLoc);
+        if (rc.getRobotCount() <= (rc.getMapWidth() * rc.getMapHeight() / 5)) {
+            if (Random.nextBoolean()) {
+                // Let's try to build a carrier.
+                // rc.setIndicatorString("Trying to build a carrier");
+                if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
+                    rc.buildRobot(RobotType.CARRIER, newLoc);
+                }
+            } else {
+                // Let's try to build a launcher.
+                rc.setIndicatorString("Trying to build a launcher");
+                if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
+                    rc.buildRobot(RobotType.LAUNCHER, newLoc);
+                }
             }
         }
     }
