014da1f8d9035281f39440d1621fff8d4901213c
LewFew <lewfew162@gmail.com>
2023-01-16 11:56:57 -0500

    Implement Well Priority (#22)

---
+++ src/v3_2/Amplifier.java
@@ -0,0 +1,31 @@
+package v3_2;
+
+import battlecode.common.*;
+
+public class Amplifier extends Robot {
+
+    static MapLocation baseHQ;
+    public static void runAmplifier(RobotController rc, int turnCount) throws GameActionException {
+        if (turnCount == 1) {
+            baseHQ = getClosestHQ(rc);
+        }
+        Direction dir = Explore.exploreAwayFromHQ(rc, baseHQ);
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        }
+
+        WellInfo[] well_info = rc.senseNearbyWells();
+        int[] island_index_info = rc.senseNearbyIslands();
+
+        //Look for islands and record
+        //TODO: Allow for overwriting, starting at the beginning. As of right now, this will lose to maps with too many islands
+        for (int i = 0; i < island_index_info.length; i++) {
+            //System.out.println("Spotted island");
+            MapLocation[] island_info = rc.senseNearbyIslandLocations(island_index_info[i]);
+            Comms.writeIslandLoc(rc, Comms.encodeIslandLoc(rc,island_info[0]),island_index_info[i]);
+        }
+        for (int i = 0; i < well_info.length; i++) {
+            Comms.writeWellLoc(rc, Comms.encodeWellLoc(rc, well_info[i].getMapLocation()), well_info[i].getResourceType());
+        }
+    }
+}
