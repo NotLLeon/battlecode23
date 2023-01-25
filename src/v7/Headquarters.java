739322a84b7e98cb567e336cab3646a9b1868e1a
Leon Liu <leonyi.liu@gmail.com>
2023-01-24 22:39:00 -0500

    Add stuff to v7

--- src/v7/Headquarters.java
+++ src/v7/Headquarters.java
@@ -2,6 +2,8 @@ package v7;
 
 import battlecode.common.*;
 
+import java.util.LinkedList;
+
 public class Headquarters extends Robot {
 
     static int index = 0;
@@ -12,6 +14,12 @@ public class Headquarters extends Robot {
     static MapLocation closeWellLoc = null;
     static boolean tryingtoBuildAnchor = false;
 
+    static LinkedList<Integer> prev_mana = new LinkedList<Integer>();
+    static LinkedList<Integer> prev_ad = new LinkedList<Integer>();
+    static int prev_mana_sum = 0;
+    static int prev_ad_sum = 0;
+    static int num_rounds_to_average = 60;
+
     public enum Symmetry {
         ROTATIONAL,
         HORIZONTAL,
@@ -21,6 +29,15 @@ public class Headquarters extends Robot {
     static void runHeadquarters(RobotController rc, int turnCount) throws GameActionException {
         // Pick a direction to build in.
         MapLocation curLoc = rc.getLocation();
+
+        rc.setIndicatorString("Ad revenue: " + Comms.getAverageAdRevenue(rc) + " Mana revenue: " + Comms.getAverageManaRevenue(rc));
+
+        int spawned_carriers = 0;
+        int spawned_launchers = 0;
+        int spawned_standard_anchors = 0;
+        int spawned_amplifiers = 0;
+        int owned_ad = rc.getResourceAmount(ResourceType.ADAMANTIUM);
+        int owned_mana = rc.getResourceAmount(ResourceType.MANA);
 
         if(turnCount == 1) {
             if(rc.getMapHeight() < 30 && rc.getMapWidth() < 30) smallMap = true;
@@ -56,18 +73,15 @@ public class Headquarters extends Robot {
 //             MapLocation island = Comms.getIsland(rc, i);
 //         }
         int currRobotCount = rc.getRobotCount();
-        RobotInfo[] nearby_enemies = rc.senseNearbyRobots(16, rc.getTeam().opponent());
+        RobotInfo[] nearby_robots = rc.senseNearbyRobots();
         int enemyLaunchers = 0;
-        for (int i = 0; i < nearby_enemies.length; i++) {
-            if (nearby_enemies[i].type == RobotType.LAUNCHER) {
+        for(RobotInfo robot : nearby_robots) {
+            if(robot.getType() == RobotType.LAUNCHER && robot.getTeam() != rc.getTeam()) {
                 enemyLaunchers++;
             }
         }
-        if (enemyLaunchers > 0) {
-            Comms.setDistressSignal(rc, true);
-        } else {
-            Comms.setDistressSignal(rc, false);
-        }
+
+        Comms.setDistressSignal(rc, enemyLaunchers > 0);
 
         for (int i = 0; i < Comms.getNumAdWells(rc); i++) {
             rc.setIndicatorDot(Comms.getAdWell(rc, i), 255,0,0);
@@ -88,6 +102,7 @@ public class Headquarters extends Robot {
                 && turnCount % 100 == 0) {
 //            rc.buildRobot(RobotType.AMPLIFIER, spawnLocs[spawnInd++]);
             buildInDir(rc, RobotType.AMPLIFIER, dirToCent);
+            spawned_amplifiers++;
         }
         if (currRobotCount > 20*hqCount
                 && turnCount >= 1000
@@ -99,6 +114,7 @@ public class Headquarters extends Robot {
         if(tryingtoBuildAnchor && rc.canBuildAnchor(Anchor.STANDARD)) {
             tryingtoBuildAnchor = false;
             rc.buildAnchor(Anchor.STANDARD);
+            spawned_standard_anchors++;
         }
 
         if(tryingtoBuildAnchor || currRobotCount > (rc.getMapHeight()*rc.getMapWidth())/5) return;
@@ -137,11 +153,66 @@ public class Headquarters extends Robot {
         while(!stop1) {
             if(buildDir1 == Direction.CENTER) stop1 = !buildInDir(rc, tryFirst, Random.nextDir());
             else stop1 = !buildInDir(rc, tryFirst, buildDir1);
+
+            if (!stop1) {
+                spawned_carriers += (tryFirst == RobotType.CARRIER) ? 1 : 0;
+                spawned_launchers += (tryFirst == RobotType.LAUNCHER) ? 1 : 0;
+            }
         }
         boolean stop2 = false;
         while(!stop2) {
             if(buildDir2 == Direction.CENTER) stop2 = !buildInDir(rc, trySecond, Random.nextDir());
             else stop2 = !buildInDir(rc, trySecond, buildDir2);
+
+            if (!stop2) {
+                spawned_carriers += (trySecond == RobotType.CARRIER) ? 1 : 0;
+                spawned_launchers += (trySecond == RobotType.LAUNCHER) ? 1 : 0;
+            }
+        }
+
+        //Putting the check here just in case.
+        //TODO:Fix this bugged code
+        if (Comms.getNumHQs(rc) > 0) {
+            int old_ad = Comms.getTotalAd(rc);
+            int old_mana = Comms.getTotalMana(rc);
+            int ad_revenue = Math.max(0, rc.getResourceAmount(ResourceType.ADAMANTIUM)
+                    + spawned_carriers * RobotType.CARRIER.getBuildCost(ResourceType.ADAMANTIUM)
+                    + spawned_launchers * RobotType.LAUNCHER.getBuildCost(ResourceType.ADAMANTIUM)
+                    + spawned_amplifiers * RobotType.AMPLIFIER.getBuildCost(ResourceType.ADAMANTIUM)
+                    + spawned_standard_anchors * Anchor.STANDARD.getBuildCost(ResourceType.ADAMANTIUM)
+                    + 2);
+            int mana_revenue = Math.max(0, rc.getResourceAmount(ResourceType.MANA)
+                    + spawned_carriers * RobotType.CARRIER.getBuildCost(ResourceType.MANA)
+                    + spawned_launchers * RobotType.LAUNCHER.getBuildCost(ResourceType.MANA)
+                    + spawned_amplifiers * RobotType.AMPLIFIER.getBuildCost(ResourceType.MANA)
+                    + spawned_standard_anchors * Anchor.STANDARD.getBuildCost(ResourceType.MANA)
+                    + 2);
+            if (Comms.getHQs(rc)[0].equals(curLoc)) {
+                Comms.setTotalAd(rc,ad_revenue);
+                Comms.setTotalMana(rc,mana_revenue);
+            } else {
+                Comms.setTotalAd(rc,old_ad + ad_revenue);
+                Comms.setTotalMana(rc,old_mana + mana_revenue);
+            }
+            if (Comms.getHQs(rc)[Comms.getNumHQs(rc)-1].equals(curLoc)) {
+                prev_mana.addLast(old_mana + mana_revenue);
+                prev_ad.addLast(old_ad + ad_revenue);
+                prev_mana_sum += prev_mana.getLast();
+                prev_ad_sum += prev_ad.getLast();
+
+                if (prev_mana.size() > num_rounds_to_average) {
+                    prev_mana_sum -= prev_mana.getFirst();
+                    prev_mana.removeFirst();
+                }
+                if (prev_ad.size() > num_rounds_to_average) {
+                    prev_ad_sum -= prev_ad.getFirst();
+                    prev_ad.removeFirst();
+                }
+                int average_ad_revenue = prev_ad_sum;// / prev_ad.size();
+                int average_mana_revenue = prev_mana_sum;// / prev_mana.size();
+                Comms.setAverageAdRevenue(rc, average_ad_revenue);
+                Comms.setAverageManaRevenue(rc, average_mana_revenue);
+            }
         }
     }
 
