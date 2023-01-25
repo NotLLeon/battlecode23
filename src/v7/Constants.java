4ff25bf0048f74160d1b54afab64dca1d9ce56b7
Leon Liu <leonyi.liu@gmail.com>
2023-01-24 22:26:09 -0500

    Init v7 with new launcher Strat (#61)
    Co-authored-by: Felix <felix.yang0917@gmail.com>

---
+++ src/v7/Constants.java
@@ -0,0 +1,28 @@
+package v7;
+
+public class Constants {
+    // weights for explore
+    static int HIGH_WEIGHT_DIRECTION = 6;
+    static int MID_WEIGHT_DIRECTION = 2;
+
+
+    // limits for explore
+    static int NUM_TRACKED_LOCATIONS = 5;
+
+    static int MOVES_TO_TRACK_LOCATION = 5;
+
+    // indices for comms
+    static int IDX_NUM_HQS = 63;
+    static int IDX_NUM_MANA_WELLS = 61;
+    static int IDX_NUM_AD_WELLS = 62;
+    static int IDX_NUM_ISLANDS = 60;
+    static int IDX_DISTRESS_SIGNAL = 59;
+
+    //Temporary
+    static int MAX_WELLS_STORED=20;
+    static int MAX_AD_WELLS_STORED = 10;
+    static int MAX_MANA_WELLS_STORED = 10;
+    static int MAX_HQS_STORED=4;
+    static int MAX_ISLANDS_STORED=20;
+
+}
